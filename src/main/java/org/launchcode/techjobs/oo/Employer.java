290b958f233b3a9ce7f0c032703519fa80214bc0
Steph Peets <stephanie.peets@gmail.com>
2021-10-18 13:33:01 -0400

    complete task 1

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -18,17 +18,17 @@ public class Employer {
         this.value = value;
     }
 
-    // Custom toString, equals, and hashCode methods:
-
     @Override
     public String toString() {
         return value;
     }
 
     @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-        if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Employer))
+            return false;
         Employer employer = (Employer) o;
         return getId() == employer.getId();
     }

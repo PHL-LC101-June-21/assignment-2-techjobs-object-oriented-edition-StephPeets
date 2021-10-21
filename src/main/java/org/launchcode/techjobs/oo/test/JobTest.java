7250c5b13feda3edfc2c6e5f0905577ccae19fa4
Steph Peets <stephanie.peets@gmail.com>
2021-10-20 21:45:20 -0400

    complete task 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,59 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+	@Test
+	public void testSettingJobId() {
+		Job job = new Job();
+		Job job2 = new Job();
+		assertNotEquals(job.getId(), job2.getId());
+	}
+
+	@Test
+	public void testJobConstructorSetsAllFields(){
+		Job job = new Job("Product tester",
+		  new Employer("ACME"),
+		  new Location("Desert"),
+		  new PositionType("Quality " + "control"),
+		  new CoreCompetency("Persistence"));
+
+		assertTrue(job instanceof Job);
+		assertTrue(job.getEmployer() instanceof Employer);
+		assertTrue(job.getLocation() instanceof Location);
+		assertTrue(job.getPositionType() instanceof PositionType);
+		assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+
+		assertEquals(job.getName(),"Product tester");
+		assertEquals(job.getEmployer().getValue(), "ACME");
+		assertEquals(job.getLocation().getValue(),"Desert");
+		assertEquals(job.getPositionType().getValue(),"Quality " + "control");
+		assertEquals(job.getCoreCompetency().getValue(),"Persistence");
+
+	}
+
+	@Test
+	public void testJobsForEquality(){
+
+		Job job = new Job("Product tester",
+		  new Employer("ACME"),
+		  new Location("Desert"),
+		  new PositionType("Quality " + "control"),
+		  new CoreCompetency("Persistence"));
+
+		Job job2 = new Job("Product tester",
+		  new Employer("ACME"),
+		  new Location("Desert"),
+		  new PositionType("Quality " + "control"),
+		  new CoreCompetency("Persistence"));
+
+		assertFalse(job.getId() == (job2.getId()));
+	}
+
+
+
 }
+
+
+
+
+	/*int id, String name, Employer employer, Location location, PositionType positionType, CoreCompetency
+	 CoreCompetency */

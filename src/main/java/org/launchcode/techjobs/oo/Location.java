c94565498d6729c485b9d2d3e5125cedbb8d3a24
Steph Peets <stephanie.peets@gmail.com>
2021-10-23 14:44:58 -0400

    complete task 6

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -1,51 +1,8 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
+public class Location extends JobField {
 
-public class Location {
-
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
-
-    public Location(String value) {
-        this();
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Location)) return false;
-        Location location = (Location) o;
-        return getId() == location.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
+	public Location(String value) {
+		super(value);
+	}
 }
--- src/main/java/org/launchcode/techjobs/oo/Main.java
+++ src/main/java/org/launchcode/techjobs/oo/Main.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-        Job job3 = new Job("", new Employer("stuff"), new Location(), new PositionType("UX"),
-        new CoreCompetency("Tasting ability"));
-        Job job4 = new Job();
-        ArrayList<Job> jobs = new ArrayList<>();
-        jobs.add(job1);
-        jobs.add(job2);
-        jobs.add(job3);
-        jobs.add(job4);
+	public static void main(String[] args) {
+		Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+		Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+		Job job3 = new Job("", new Employer("stuff"), new Location(""), new PositionType("UX"),
+		new CoreCompetency("Tasting ability"));
+		Job job4 = new Job();
 
-        for (Job job : jobs){
-            System.out.println(job);
-        }
-}
+		ArrayList<Job> jobs = new ArrayList<>();
+		jobs.add(job1);
+		jobs.add(job2);
+		jobs.add(job3);
+		jobs.add(job4);
+
+		for (Job job : jobs) {
+			System.out.println(job);
+		}
+	}
 
 }
